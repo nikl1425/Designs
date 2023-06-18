28d954288866ba66322cd798908a373c6a2fd086
nikl1425 <nmhjort7@gmail.com>
2023-06-18 23:05:15 +0200

    card features

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     fontFamily: {
       'Primary': [ 'Inter var', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue,Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
     },
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1700px',
+      },
+    },
   },
   plugins: [],
 }
