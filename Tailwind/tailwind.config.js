6faab227096d9c7678dc362838e5eefd117509da
nikl1425 <nmhjort7@gmail.com>
2023-06-18 13:34:47 +0200

    tail

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'Primary': [ 'Inter var', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue,Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
